b6fba70590e69500b717935f9d95e4df8a1a9b72
Jérôme LELEU <leleuj@gmail.com>
2026-07-15 12:31:26 +0200

    add openid federation endpoint

--- src/main/java/org/pac4j/demos/Application.java
+++ src/main/java/org/pac4j/demos/Application.java
@@ -1,7 +1,12 @@
 package org.pac4j.demos;
 
+import org.pac4j.core.config.Config;
+import org.pac4j.core.exception.http.HttpAction;
 import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.oidc.client.OidcClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +16,9 @@ public class Application {
 
     @Autowired
     private ProfileManager profileManager;
+
+    @Autowired
+    private Config config;
 
     @RequestMapping("/")
     @ResponseBody
@@ -24,5 +32,17 @@ public class Application {
     public String secure() {
         return "<h1>Protected area</h1><a href='/'>Home</a><p/>"
                 + "<p><a href='/logout'>Logout</a></p>" + profileManager.getProfiles();
+    }
+
+    @RequestMapping(value = "/.well-known/openid-federation")
+    @ResponseBody
+    public ResponseEntity<String> oidcFederation() throws HttpAction {
+        var oidcClient = (OidcClient) config.getClients().findClient("OidcClient").get();
+        var generator = oidcClient.getConfiguration().getFederation().getEntityConfigurationGenerator();
+        var entityStatement = generator.generateEntityStatement();
+        var contentType = generator.getContentType();
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(entityStatement);
     }
 }
